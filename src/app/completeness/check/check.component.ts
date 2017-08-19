2f5b88bdf7510aeda205d0ab52f96d4a79aa0aba
Hugo Hernani <hugo@quezmedia.com>
2017-08-18 23:15:19 -0300

    Adding Basic CheckComponent. Unfinished Yet

---
+++ src/app/completeness/check/check.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'completeness-check',
+  templateUrl: './check.component.html',
+  styleUrls: ['./check.component.css']
+})
+export class CheckComponent implements OnInit {
+  @Input() resource: any;
+  @Output() onCompletePercentage = new EventEmitter<integer>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    // TODO Watch Changes At Resource
+    // See https://angular.io/guide/component-interaction#intercept-input-property-changes-with-ngonchanges
+
+    // Emit The Percentage Value When Changes Are Made
+    this.onCompletePercentage.emit(getCompletedPercentage());
+  }
+
+  getCompletedPercentage(): integer{
+
+  }
+
+}
